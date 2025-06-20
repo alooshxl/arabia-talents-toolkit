d481bd3f86405028537ad2f1cca077b7eec17b03
alooshxl <alialchami7@gmail.com>
2025-06-20 15:48:27 +0300

    Improve chat language logic and model

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import ArabiaTalentsAI from '../ArabiaTalentsAI';
 
 export default function Layout() {
   return (
@@ -13,6 +14,7 @@ export default function Layout() {
           <Outlet />
         </main>
       </div>
+      <ArabiaTalentsAI />
     </div>
   );
 }
